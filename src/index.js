c67ad3afd1e333502b4405d647a160bfa110a6bc
cursoprogramacionuo <130976798+cursoprogramacionuo@users.noreply.github.com>
2024-10-31 13:04:28 +0100

    hotjar

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import Hotjar from '@hotjar/browser';
 import { Provider } from "react-redux";
 import store from "./Reducers/store"
 
-const siteId = 5191130;
-const hotjarVersion = 6;
+//const siteId = 5191130;
+//const hotjarVersion = 6;
 
-Hotjar.init(siteId, hotjarVersion);
+//Hotjar.init(siteId, hotjarVersion);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
